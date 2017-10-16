bb4f76ac0735d330fa6646150e74d4a2863f5824
pusalieth <johnpring09@gmail.com>
2017-10-15 22:58:09 -0700

    Web Server Test

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,17 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+function createDir() {
+    var myObject, newfolder;
+    myObject = new ActiveXObject("Scripting.FileSystemObject");
+    newfolder = myObject.CreateFolder("/home/jake/GitHub/Automated-Teaching-Distributor");
+    alert('Dir created');
+}
+
+function createFile() {
+    var htmlString = "<? php $file = fopen(\"test.txt\",\"w\"); echo fwrite($file,\"Hello World. Testing!\"); fclose($file); ?>";
+    alert(htmlString);
+    alert('create File');
+}
+
+function addHeader() {
+    var myHeading = document.querySelector('h1');
+    myHeading.textContent = 'Hello world! Script Succesful';
+}
