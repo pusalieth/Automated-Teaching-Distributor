9f361ad7113b44c9bff9d31d99d495aa7001c0b7
pusalieth <johnpring09@gmail.com>
2017-10-16 01:19:36 -0700

    WebHook

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <!DOCTYPE HTML>
 <html>
+<script>
+  require 'sinatra'
+  require 'json'
+
+  post '/payload'
+  do
+    push = JSON.parse(request.body.read)
+  puts "I got some JSON: #{push.inspect}"
+  end
+</script>
+
 <script src="scripts/main.js"></script>
 
 <head>
-    <title>Sample "Hello, World" Application</title>
+  <title>Sample "Hello, World" Application</title>
 </head>
 
 
@@ -13,26 +24,26 @@
 
 <body bgcolor=white>
 
-    <table border="0" cellpadding="10">
-        <tr>
-            <td>
-                <img src="images/springsource.png">
-            </td>
-            <td>
-                <h1>Sample "Hello, World" Application</h1>
-            </td>
-        </tr>
-    </table>
+  <table border="0" cellpadding="10">
+    <tr>
+      <td>
+        <img src="images/springsource.png">
+      </td>
+      <td>
+        <h1>Sample "Hello, World" Application</h1>
+      </td>
+    </tr>
+  </table>
 
-    <p>This is the home page for the HelloWorld Web application. </p>
-    <p>To prove that they work, you can execute either of the following links:
-        <ul>
-            <li>To a
-                <a href="hello.jsp">JSP page</a>.
-                <li>To a
-                    <a href="hello">servlet</a>.
-        </ul>
-    </p>
+  <p>This is the home page for the HelloWorld Web application. </p>
+  <p>To prove that they work, you can execute either of the following links:
+    <ul>
+      <li>To a
+        <a href="hello.jsp">JSP page</a>.
+        <li>To a
+          <a href="hello">servlet</a>.
+    </ul>
+  </p>
 </body>
 
 </html>
