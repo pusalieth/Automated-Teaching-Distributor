a385278e0ad6beeb09e28b818bc733dd2bc59563
pusalieth <johnpring09@gmail.com>
2017-10-17 00:04:08 -0700

    PHP test

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE HTML>
 <html>
+
 <script>
   require 'sinatra'
   require 'json'
@@ -11,7 +12,24 @@
   end
 </script>
 
-<script src="scripts/main.js"></script>
+<script>
+  function createDir() {
+    var htmlString = "<?php mkdir(\"testing\"); ?>";
+    alert(htmlString);
+    alert('Dir created');
+  }
+
+  function createFile() {
+    var htmlString = "<? php $file = fopen(\"test.txt\",\"w\"); echo fwrite($file,\"Hello World. Testing!\"); fclose($file); ?>";
+    alert(htmlString);
+    alert('create File');
+  }
+
+  function addHeader() {
+    var myHeading = document.querySelector('h1');
+    myHeading.textContent = 'Hello world! Script Succesful';
+  }
+</script>
 
 <head>
   <title>Sample "Hello, World" Application</title>
