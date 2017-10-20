7b5a6980d08b180c0317ac3a8877becc2b67a72b
Seth Brunwasser <sbrunwasser1998@gmail.com>
2017-10-19 23:08:49 -0700

    Adding jszip
    Test for directory and file upload

--- index.php
+++ index.php
@@ -12,6 +12,29 @@
   end
 </script>
 
+
+// Javascript using JSZip to create directories
+echo "
+<script src="https://stuk.github.io/jszip/dist/jszip.min.js" type = 'text/javascript'>
+  if (window.File && window.FileReader && window.FileList){
+    // APIs supported in the browser
+  } else {
+    alert('The File Upload APIs are not fully supported in this browser');
+  }
+
+  var JSZip = require("jszip");
+  var zip = JSZip();
+
+  homeworkFolder = zip.folder("Homework")
+  honorsFolder = zip.folder("Honors")
+  zip.folder("Lecture Audio")
+  zip.folder("Lecture Slides")
+  zip.folder("Course Information")
+
+
+
+</script>;"
+
 <script>
   function createDir() { < button type = "button" > Click Me! < /button>
     <?php mkdir(test); ?>
@@ -41,7 +64,25 @@
   <form>
     First name:
     <br>
-    <input type="text" name="firstname">
+    <input type="file" id="files" name="files[]" multiple />
+    <output id="list"></output>
+    echo "<script>
+        function handleFileSelect(evt) {
+          var files = evt.target.files; // FileList object
+
+          // files is a FileList of File objects. List some properties.
+          var output = [];
+          for (var i = 0, f; f = files[i]; i++) {
+            output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                        f.size, ' bytes, last modified: ',
+                        f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                        '</li>');
+          }
+          document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+        }
+
+        document.getElementById('files').addEventListener('change', handleFileSelect, false);
+      </script>"
     <br> Last name:
     <br>
     <input type="text" name="lastname">
